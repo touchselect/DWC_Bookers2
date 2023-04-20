0a3051480a47ebcc8d5d731350fff3b8794b10f6
touchselect <kir191474@gmail.com>
2023-04-20 14:18:16 +0000

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/edit'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get 'homes/top'
+  root to: 'homes#top'
+  get 'homes/about' => 'homes#about'
   devise_for :users
+  resources :users, only: [:index, :show, :edit, :update]
+  resources :books, only: [:index, :create, :show, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
