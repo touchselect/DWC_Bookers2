b60b5313a0523289b3e04b0e803e0aa8262a758d
touchselect <kir191474@gmail.com>
2023-04-20 13:52:21 +0000

    install bootstrap

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,15 @@
 const { environment } = require('@rails/webpacker')
 
 module.exports = environment
+
+const webpack = require('webpack');
+
+module.exports = {
+	plugins: [
+		new webpack.ProvidePlugin({
+			$: 'jquery',
+			jQuery: 'jquery',
+			Popper: ['popper.js', 'default']
+		})
+	]
+};
