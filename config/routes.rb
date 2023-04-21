8365fe74fa4977532ac45f9eba9cab16acafc3fa
touchselect <kir191474@gmail.com>
2023-04-21 17:09:12 +0000

    create books edit update

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'homes/about' => 'homes#about'
   devise_for :users
   resources :users, only: [:index, :show, :edit, :update]
-  resources :books, only: [:index, :create, :show, :edit, :destroy]
+  resources :books, only: [:index, :create, :show, :edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
