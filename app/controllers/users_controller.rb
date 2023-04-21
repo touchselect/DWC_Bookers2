468b53ffd0476b9cc69705222b41ca15562ce04e
touchselect <kir191474@gmail.com>
2023-04-21 18:29:54 +0000

    create user edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :body, :profile_image)
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,10 +1,10 @@
 class Book < ApplicationRecord
-	has_one_attached :image
 	belongs_to :user
+	has_one_attached :image
 
 	def get_image
 		unless image.attached?
-			file_path = Rails.root.join('app/assets/images/default-image.jpg')
+			file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
 			image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
 		end
 		image
